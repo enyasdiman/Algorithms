978a4674cbe5249a8b82056d16fea6864d77dab7
EDA NUR YASDIMAN <enyasdiman@gmail.com>
2020-01-13 01:42:45 +0300

    Update lin‌kedList.c

--- lin‌kedList.c
+++ lin‌kedList.c
@@ -53,3 +53,88 @@ main() {
     }
 }
 
+int add(Linkedlist *sth) {
+    if(first!=NULL) {
+        last->back=NULL;
+    }
+    else {
+        first=sth;
+        last=sth;
+        first->back=NULL;
+    }
+    return 0;
+}
+
+int Listing() {
+    Linkedlist *p;
+    p=first;
+    if(p==NULL)
+        return -1;
+    
+    while(p!=NULL) {
+        writeScreen(p);
+        p=p->back;
+    }
+    return 0;
+}
+
+Linkedlist *Searching(int lp) {
+    Linkedlist *p;
+    p=first;
+    while(p!=NULL) {
+        if(p->info=lp)
+            return p;
+        p=p->back;
+    }
+    return NULL;
+}
+
+Linkedlist *delete(int wd) {
+    Linkedlist *p, *previous;
+    p=first;
+    previous=NULL;
+    
+    while(p!=NULL) {
+        if(wb==p->info)
+            break;
+        previous=p;
+        p=p->back;
+    }
+    
+    if(p!=NULL) {
+        if(previous==NULL) {
+            if(first==last) {
+                first=NULL;
+                last=NULL;
+            }
+            else
+                first=first->back;
+        }
+        else {
+            previous->back=p->back;
+            if(previous->back==NULL)
+                last=previous;
+        }
+        free(p);
+        return p;
+    }
+    else
+        return NULL;
+}
+
+void writeScreen(Linkedlist *wr) {
+    printf("info:%d, msg:%s\n", wr->info, wr->msg);
+}
+
+Linkedlist *readKeyboard() {
+    Linkedlist *r;
+    r=malloc(sizeof(Linkedlist));
+    if(r==NULL)
+        return NULL;
+    
+    puts("Please add an information:");
+    scanf("%d", &(r->info));
+    puts("Please write a message:");
+    scanf("%s", r->msg);
+    return r;
+}
